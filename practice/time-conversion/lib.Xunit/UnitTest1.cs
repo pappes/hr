5f6acf8746c678c79304a37ab174a42017eb1398
pappes <pappes@gmail.com>
2018-01-07 21:24:53 +1000

    remove commented code

--- practice/time-conversion/lib.Xunit/UnitTest1.cs
+++ practice/time-conversion/lib.Xunit/UnitTest1.cs
@@ -16,28 +16,5 @@ namespace lib.Xunit
         {
             Assert.Equal(result, Solution.TestHarness(inp));
         }
-    }/* *
-    public class UnitTestFacts
-    {
-        public int[] createArray(int size, int defaultValue)
-        {
-            int[] mdArray = new int[size];
-            for (int i=0; i<size; i++) {
-                mdArray[i]=defaultValue;
-            }
-            return mdArray;
-        }
-        [Fact]
-        public void TestDiaginalDiff1millionx1million()
-        {
-            int maxArray = 100000;  ///problem defines maximum 10^5 candles
-            int[] bigArray = createArray(maxArray, Int32.MaxValue);
-            Assert.Equal(maxArray, Solution.TestHarness(maxArray, bigArray));
-            bigArray[0]=0;
-            Assert.Equal(maxArray-1, Solution.TestHarness(maxArray, bigArray));
-            bigArray = createArray(maxArray, Int32.MinValue);
-            bigArray[0]=0;
-            Assert.Equal(1, Solution.TestHarness(maxArray, bigArray));
-        }
-    }*/
+    }
 }
