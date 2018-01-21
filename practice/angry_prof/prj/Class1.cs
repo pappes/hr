74210938b0d82ecb68b46e12e8726b8c133b47d6
pappes <pappes@gmail.com>
2018-01-21 15:37:33 +1000

    remove commented out observable code

--- practice/angry_prof/prj/Class1.cs
+++ practice/angry_prof/prj/Class1.cs
@@ -4,16 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 
-
-/* 
-using System;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Reactive; 
-using System.Reactive.Linq; */
-
-
-
 public class Solution {
 //https://www.hackerrank.com/challenges/angry-professor/problem
     public static void TestHarness(StreamReader input, StreamWriter output)
@@ -40,6 +30,17 @@ public class Solution {
         for(int a0 = 0; a0 < tests; a0++){
             destination.WriteLine(result[a0]);        
         }*/
+      ScheduledClass lessonProvider = new ScheduledClass(10,7);
+      Professor lessonObserver = new Professor();
+      lessonObserver.Subscribe(lessonProvider);
+
+      lessonProvider.RecordArrival(-1);
+            destination.WriteLine(lessonObserver.angry);  
+      lessonProvider.RecordArrival(1);
+      lessonProvider.RecordArrival(1);
+      lessonProvider.RecordArrival(1);
+      lessonProvider.RecordArrival(1);
+            destination.WriteLine(lessonObserver.angry);  
             destination.WriteLine(tests); 
             destination.WriteLine("no class for you");  
             destination.WriteLine("no class today");  
@@ -57,32 +58,13 @@ class LectureTheatreDTO {
     public int late { get; set; } = 0;   
     public int size { get; set; }
     public int threshold { get; set; }
-
-    // public LectureTheatre () {
-    // }
-/* 
-    public void RecordArrival (int time) {
-        time<=0 ? onTime++ : late++;
-        if (onTime > size-threshold) {
-            cancelled = 'NO'
-        } else {
-        if (late > size-threshold) {
-            cancelled = 'YES'            
-        }
-    } */
 }
 
 class ScheduledClass : IObservable<LectureTheatreDTO> {   
-    private List<IObserver<LectureTheatreDTO>> staff; 
-    private LectureTheatreDTO lesson;
-
-    /*public string cancelled { get; set; } = "?";
-    int size { get; set; }
-    int threshold { get; set; }*/
+    private List<IObserver<LectureTheatreDTO>> staff = new List<IObserver<LectureTheatreDTO>>(); 
+    private LectureTheatreDTO lesson = new LectureTheatreDTO();
 
     public ScheduledClass (int classSize, int classThreshold) {
-        staff = new List<IObserver<LectureTheatreDTO>>();
-        lesson = new LectureTheatreDTO();
         lesson.size = classSize;
         lesson.threshold = classThreshold;
     }
@@ -104,46 +86,21 @@ class ScheduledClass : IObservable<LectureTheatreDTO> {
         }
         return new Unsubscriber<LectureTheatreDTO>(staff, lecturer);
     }
-/* 
-    public void RecordArrival (int time) {
-        time<=0 ? onTime++ : late++;
-        if (onTime > size-threshold) {
-            cancelled = 'NO'
-        } else {
-        if (late > size-threshold) {
-            cancelled = 'YES'            
-        }
-    } */
 }
 
 class Professor : IObserver<LectureTheatreDTO> {    
     public string angry { get; set; } = "?";
     private IDisposable subscription;
-    //Sitting classRoomSitting;
 
-   /*  public Professor (int classSize, int classThreshold) {
-        size = classSize;
-        threshold = classThreshold;
-    } */
- 
-   /* public void StudentArrival (Sitting s) {
-        classRoomSitting = s;
-        if (s.onTime > s.threshold) {
-            s.cancelled = "NO";
-        } else {
-            if (s.late > s.size-s.threshold) {
-                s.cancelled = "YES" ;           
-            } 
-        } 
-    }*/
    public virtual void OnNext(LectureTheatreDTO plannedClass) 
    {
         if (plannedClass.onTime > plannedClass.threshold) {
             this.angry = "NO";
         } else {
             if (plannedClass.late > plannedClass.size-plannedClass.threshold) {
-                this.angry = "YES" ;     
-                Unsubscribe();      
+                this.angry = "YES" ;   
+                //TODO: investigate why unsubscribing causes problems    Would it cause unsubscribe to be called twice?
+                //Unsubscribe();  //causes thread isues    
             } 
         } 
    }
@@ -156,169 +113,20 @@ class Professor : IObserver<LectureTheatreDTO> {
    public virtual void Unsubscribe()
    {
       subscription.Dispose();
-      //flightInfos.Clear();
    }
 
    public virtual void OnCompleted() 
    {
-      //flightInfos.Clear();
+      // No implementation.
    }
 
-   // No implementation needed: Method is not called by the BaggageHandler class.
+   // No implementation needed: Method is not called by the ScheduledClass class.
    public virtual void OnError(Exception e)
    {
       // No implementation.
    }
 }
 
-
-/* class IObservableSitting<T> : IObservable<T> where T:class {    
-    // public string cancelled { get; set; } = "?";
-    // int size { get; set; }
-    // int threshold { get; set; }
-    // int onTime { get; set; } = 0;
-    // int late { get; set; } = 0; 
-
-    
-    private List<IObserver<T>> observers = new List<IObserver<T>>();
-    #region IObservable<T> Members
-        public IDisposable Subscribe(IObserver<T> observer)
-        {
-            if (!observers.Contains(observer))
-                observers.Add(observer);
-            return new Unsubscriber<T>(observers, observer);
-        }
-    #endregion
-
-    // public Sitting (int classSize, int classThreshold) {
-    //     size = classSize;
-    //     threshold = classThreshold;
-    // } 
- 
-    // public void RecordArrival (int time) {
-    //     time<=0 ? onTime++ : late++;
-    //     if (onTime > size-threshold) {
-    //         cancelled = 'NO'
-    //     } else {
-    //     if (late > size-threshold) {
-    //         cancelled = 'YES'            
-    //     }
-    // } 
-}
-
-class IObserverProfessor<T> : IObserver<T> where T:class, new() {    
-    public string angry { get;  } = "?";
-
-    private IDisposable unsubscriber;
-    #region IObserver<T> Members\
-        public void OnCompleted()
-        {
-            //Console.WriteLine("{0} : OnComplete is called.", this.Name);
-            this.Unsubscribe();
-        }
-
-        public void OnError(Exception error)
-        {
-            //Console.WriteLine("{0} : OnError is called", this.Name);
-            this.LogException(error);
-        }
-
-        public void OnNext(T value)
-        {
-            //Console.WriteLine("{0} : OnNext is called.", this.Name);
-            //this.StudentArrival(value);
-        }
-    #endregion    
-
-    public virtual void Subscribe(IObservable<T> observable)
-    {
-        if(observable != null)
-            this.unsubscriber = observable.Subscribe(this);
-    }
-
-    public virtual void Unsubscribe()
-    {
-        //Console.WriteLine("{0} : Calling Unsubscriber for Observer", this.Name);
-        if(this.unsubscriber != null)
-            this.unsubscriber.Dispose();
-    }
-
-    private void LogException(Exception error)
-    {
-        //Console.WriteLine("Exception occurred while traversing thorough objects of type {0}", error.GetType().Name);
-        Console.WriteLine("Exception Message : {0}", error.Message);
-    }
-} */
-
-
-
-/* 
-//sample code from http://www.abhisheksur.com/2010/08/implementation-of-observer.html
-//want a portable observable implementation becasue hackerrank does not allow asemblies
-public class SampleObserver<T> : IObserver<T> where T:class, new()
-{
-    private IDisposable unsubscriber;
-
-    public string Name { get; set; }
-
-    public SampleObserver(string name)
-    {
-        this.Name = name;
-    }
-
-    #region IObserver<T> Members\
-        public void OnCompleted()
-        {
-            Console.WriteLine("{0} : OnComplete is called.", this.Name);
-            this.Unsubscribe();
-        }
-
-        public void OnError(Exception error)
-        {
-            Console.WriteLine("{0} : OnError is called", this.Name);
-            this.LogException(error);
-        }
-
-        public void OnNext(T value)
-        {
-            Console.WriteLine("{0} : OnNext is called.", this.Name);
-            this.LogProperties(value);
-        }
-    #endregion
-
-    public virtual void Subscribe(IObservable<T> observable)
-    {
-        if(observable != null)
-            this.unsubscriber = observable.Subscribe(this);
-    }
-
-    public virtual void Unsubscribe()
-    {
-        Console.WriteLine("{0} : Calling Unsubscriber for Observer", this.Name);
-        if(this.unsubscriber != null)
-            this.unsubscriber.Dispose();
-    }
-
-    private void LogException(Exception error)
-    {
-        Console.WriteLine("Exception occurred while traversing thorough objects of type {0}", error.GetType().Name);
-        Console.WriteLine("Exception Message : {0}", error.Message);
-    }
-    private void LogProperties(T value)
-    {
-        T tobj = value;
-        PropertyInfo[] pinfos = tobj.GetType().GetProperties();
-        Console.WriteLine("==========================={0}====================================================", this.Name);
-        Console.WriteLine("Lets trace all the Properties ");
-
-        foreach (PropertyInfo pinfo in pinfos)
-            Console.WriteLine("Value of {0} is {1}", pinfo.Name, pinfo.GetValue(tobj, null));
-
-        Console.WriteLine("============================={0}===================================================", this.Name);
-
-            
-    }
-} */
 
 //Sample code from 
 //https://docs.microsoft.com/en-us/dotnet/standard/events/observer-design-pattern
@@ -328,7 +136,7 @@ public class Unsubscriber<T> :IDisposable
     private List<IObserver<T>> _observers;
     private IObserver<T> _observer;
 
-    //TODO: use callback to unubscribe
+    //TODO: use callback to unsubscribe
     public Unsubscriber(List<IObserver<T>> observers, IObserver<T> observer)
     {
         this._observers = observers;
@@ -344,39 +152,3 @@ public class Unsubscriber<T> :IDisposable
     #endregion
 }
 
-
-/* public class SampleObservable<T> : IObservable<T> where T:class
-{
-
-    private List<IObserver<T>> observers = new List<IObserver<T>>();
-      
-    #region IObservable<T> Members
-        public IDisposable Subscribe(IObserver<T> observer)
-        {
-            if (!observers.Contains(observer))
-                observers.Add(observer);
-            return new Unsubscriber<T>(observers, observer);
-        }
-    #endregion
-
-    public void TrackObserver(T obj)
-    {
-        if (obj != null)
-        {
-            foreach (IObserver<T> observer in observers)
-                if (observers.Contains(observer))
-                    observer.OnNext(obj);
-                else
-                    observer.OnError(new ApplicationException("Not available"));
-        }
-    }
-
-    public void TrackComplete()
-    {
-        foreach (IObserver<T> observer in observers)
-            observer.OnCompleted();
-
-        observers.Clear();
-    } 
-}*/
-        
