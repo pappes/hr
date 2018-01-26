e6c02a74ff159d6ca77f97aca5bc291d42c6f44d
pappes <pappes@gmail.com>
2018-01-26 19:28:25 +1000

    refactor for testability

--- practice/angry_prof/prj/Class1.cs
+++ practice/angry_prof/prj/Class1.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 
+
 public class Solution {
 //https://www.hackerrank.com/challenges/angry-professor/problem
     public static void TestHarness(StreamReader input, StreamWriter output) {
@@ -39,98 +40,117 @@ public class Solution {
 }
 
 class LectureTheatreDTO {    
-    public int onTime { get; set; } = 0;
-    public int late { get; set; } = 0;   
-    public int size { get; set; }
-    public int threshold { get; set; }
+    public int onTimeStudents { get; set; } = 0;
+    public int lateStudents { get; set; } = 0;   
+    public int classSize { get; set; }
+    public int cancellationThreshold { get; set; }
 }
 
 class ScheduledClass : IObservable<LectureTheatreDTO> {   
-    private List<IObserver<LectureTheatreDTO>> staff = new List<IObserver<LectureTheatreDTO>>(); 
-    private LectureTheatreDTO lesson = new LectureTheatreDTO();
+    private List<IObserver<LectureTheatreDTO>> _staff; 
+    private LectureTheatreDTO _lesson ;
 
-    public ScheduledClass (int classSize, int classThreshold) {
-        lesson.size = classSize;
-        lesson.threshold = classThreshold;
+    public ScheduledClass (int expectedClassSize, int classCancellationThreshold) {
+        _staff = new List<IObserver<LectureTheatreDTO>>();
+        _lesson = new LectureTheatreDTO();
+        InitialiseStatistics(_lesson, expectedClassSize, classCancellationThreshold);
     }
     public void RecordArrival (int arrivalTime) {
-        if (arrivalTime<=0)
-            lesson.onTime++;
-        else
-            lesson.late++;        
-        //copy the list before enumerating in case one observer ragequits when they see what thet don't like
-        List<IObserver<LectureTheatreDTO>> observers = new List<IObserver<LectureTheatreDTO>>(staff);
-        foreach (var lecturer in observers)
-            lecturer.OnNext(lesson);
+        UpdateStatistics(_lesson, arrivalTime);        
+        NotifyStaff(_staff,_lesson);
     }
     #region IObservable Members
         public IDisposable Subscribe(IObserver<LectureTheatreDTO> lecturer)
         {
-            // Check whether lecturer is already registered. If not, add it
-            if (! staff.Contains(lecturer)) {
-                staff.Add(lecturer);
-                // Provide observer with existing data.
-                lecturer.OnNext(lesson);
-            }
-            return new Unsubscriber<LectureTheatreDTO>(staff, lecturer);
+            RecordSubscription(_staff, lecturer);            
+            lecturer.OnNext(_lesson);// Provide observer with existing data.
+            return CreateUnsubscriber(_staff, lecturer);
         }
     #endregion IObservable Members
+
+    private static void InitialiseStatistics (LectureTheatreDTO lesson, int expectedClassSize, int classCancellationThreshold) {
+        lesson.classSize = expectedClassSize;
+        lesson.cancellationThreshold = classCancellationThreshold;
+    }
+    private static void UpdateStatistics (LectureTheatreDTO lesson, int arrivalTime) {
+        if (arrivalTime<=0)
+            lesson.onTimeStudents++;
+        else
+            lesson.lateStudents++;   
+    }
+    private static void NotifyStaff (List<IObserver<LectureTheatreDTO>> staff, LectureTheatreDTO lesson) {
+        //copy the list before enumerating in case one observer ragequits when they see what thet don't like
+        List<IObserver<LectureTheatreDTO>> staffClone = new List<IObserver<LectureTheatreDTO>>(staff);
+        foreach (var lecturer in staffClone)
+            lecturer.OnNext(lesson);
+    }
+    //private static void RecordSubscription (List<IObserver<object>> staff, IObserver<object> lecturer) {
+    private static void RecordSubscription (List<IObserver<LectureTheatreDTO>> staff, IObserver<LectureTheatreDTO> lecturer) {
+        // Check whether lecturer is already registered. If not, add it
+        if (! staff.Contains(lecturer)) {
+            staff.Add(lecturer);
+        }
+    }
+    private static void Unsubscribe (List<IObserver<LectureTheatreDTO>> staff, IObserver<LectureTheatreDTO> lecturer) {
+        if (staff.Contains(lecturer)) 
+            staff.Remove(lecturer);
+    }
+    private static IDisposable CreateUnsubscriber (List<IObserver<LectureTheatreDTO>> staff, IObserver<LectureTheatreDTO> lecturer) {
+        return new UnsubscriberLambda(() => Unsubscribe(staff, lecturer) );
+    }
 }
 
 class Professor : IObserver<LectureTheatreDTO> {    
     public string angry { get; set; } = "?";
-    private IDisposable subscription;
+    private IDisposable _subscription;
 
+    public void Subscribe(ScheduledClass plannedClass) {
+        SubscribeT(ref _subscription, plannedClass, this);
+    }
     #region IObserver Members
         public virtual void OnNext(LectureTheatreDTO plannedClass) {
-            if (plannedClass.onTime >= plannedClass.threshold) {
-                this.angry = "NO"; 
-                //stop watching and get on with the job, even if it causess problems for list enumeration
-                Unsubscribe();  
-            } else {
-                if (plannedClass.late >0 && plannedClass.late >= plannedClass.size-plannedClass.threshold) {
-                    this.angry = "YES" ;   
-                    //ragequit in protest, even if it causess problems for list enumeration
-                    Unsubscribe();  
-                } 
-            } 
+            if (ConfirmAttendance(plannedClass)) Unsubscribe();
         }
         public virtual void OnCompleted() {} // No implementation.
-
-        // No implementation needed: Method is not called by the ScheduledClass class.
         public virtual void OnError(Exception e){} // No implementation.
-    #endregion IObserver Members
+    #endregion IObserver Members    
 
-    public virtual void Subscribe(ScheduledClass plannedClass) {
-        subscription = plannedClass.Subscribe(this);
+    private bool ConfirmAttendance(LectureTheatreDTO plannedClass) {
+        if (plannedClass.onTimeStudents >= plannedClass.cancellationThreshold) {
+            this.angry = "NO";             
+            return true; //stop watching and get on with the job (even if it causess problems for list enumeration)
+        } else {
+            if (plannedClass.lateStudents >0 && 
+                plannedClass.lateStudents >= plannedClass.classSize-plannedClass.cancellationThreshold) {
+                this.angry = "YES" ;                  
+                return true; //ragequit in protest( even if it causess problems for list enumeration)
+            } 
+        } 
+        return false; //attendence incomplete
     }
-
-    public virtual void Unsubscribe() {
+    private void UnsubscribeT(IDisposable subscription) {
         subscription.Dispose();
+    }
+    private void SubscribeT(ref IDisposable subscription, ScheduledClass plannedClass, Professor subscriber) {
+        subscription = plannedClass.Subscribe(subscriber);
+    }
+    private void Unsubscribe() {
+        UnsubscribeT(_subscription);
     }
 }
 
-//Sample code from 
-//https://docs.microsoft.com/en-us/dotnet/standard/events/observer-design-pattern
-// AND http://www.abhisheksur.com/2010/08/implementation-of-observer.html
-public class Unsubscriber<T> :IDisposable
+public class UnsubscriberLambda :IDisposable
 {
-    private List<IObserver<T>> _observers;
-    private IObserver<T> _observer;
+    private Action _disposeCallback;
 
-    //TODO: use callback to unsubscribe
-    public Unsubscriber(List<IObserver<T>> observers, IObserver<T> observer)
+    public UnsubscriberLambda(Action callback)
     {
-        this._observers = observers;
-        this._observer = observer;
+        _disposeCallback = callback;
     }
     #region IDisposable Members
         public void Dispose()
         {
-            //Remove the Observer from the list when the subscriber disposes the diposable.
-            if (_observer != null && _observers.Contains(_observer))
-                _observers.Remove(_observer);
+            _disposeCallback();
         }
     #endregion
 }
-
