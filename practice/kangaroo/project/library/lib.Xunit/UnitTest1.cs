f51229887f855661a87545075d04fd6690ac0962
pappes <pappes@gmail.com>
2018-03-24 22:11:28 +1000

    brute force implementation of kangaroo problem

--- practice/kangaroo/project/library/lib.Xunit/UnitTest1.cs
+++ practice/kangaroo/project/library/lib.Xunit/UnitTest1.cs
@@ -11,8 +11,8 @@ namespace lib.Xunit
 
 
         [Theory]
-        [InlineData("NO", "1\r\n1 1\r\n0 0")]
-        [InlineData("NO", "1\r\n1 1\r\n0")]
+        [InlineData("YES", "0 3 4 2")]
+        [InlineData("NO", "0 2 5 3")]
 
         public void TestProff(string expectedResult, string testData)
         {
@@ -21,4 +21,68 @@ namespace lib.Xunit
         }
 
     }
+    public class UnitTestFacts : TestHelper
+    {
+        public UnitTestFacts (ITestOutputHelper output)
+        : base(output) { }
+
+
+        [Fact]
+        public void TestEqEq()
+        {            
+            var k1 = new Kangaroo(3,4);
+            var k2 = new Kangaroo(3,4);
+            Assert.True(k1==k2);
+            Assert.True(k1.Equals(k2));
+            k1.Move();
+            k2.Move();
+            Assert.True(k1==k2);
+            Assert.True(k1.Equals(k2));
+        }
+
+        [Fact]
+        public void TestNotEq()
+        {            
+            var k1 = new Kangaroo(3,4);
+            var k2 = new Kangaroo(2,4);
+            Assert.True(k1!=k2);
+            k1.Move();
+            k2.Move();
+            Assert.True(k1!=k2);
+        }
+
+        [Fact]
+        public void TestLt()
+        {            
+            var k1 = new Kangaroo(1,4);
+            var k2 = new Kangaroo(2,4);
+            Assert.True(k1<k2);
+            k1.Move();
+            k2.Move();
+            Assert.True(k1<k2);
+        }
+
+        [Fact]
+        public void TestGt()
+        {            
+            var k1 = new Kangaroo(3,4);
+            var k2 = new Kangaroo(2,4);
+            Assert.True(k1>k2);
+            k1.Move();
+            k2.Move();
+            Assert.True(k1>k2);
+        }
+
+        [Fact]
+        public void TestMove()
+        {            
+            var k1 = new Kangaroo(3,4);
+            var k2 = new Kangaroo(2,6);
+            Assert.True(k1>k2);
+            k1.Move();
+            k2.Move();
+            Assert.True(k1<k2);
+        }
+    }
 }
+  
--- practice/kangaroo/project/library/prj/Class1.cs
+++ practice/kangaroo/project/library/prj/Class1.cs
@@ -10,7 +10,19 @@ public class Solution {
     }
     static string kangaroo(int x1, int v1, int x2, int v2) {
         // Complete this function
-        return "";
+        var k1 = new Kangaroo(x1,v1);
+        var k2 = new Kangaroo(x2,v2);
+        var first = k1>k2 ? k1 : k2;
+        var last =  k2>k1 ? k1 : k2;
+        if (last == first) return "YES";
+        if (last.SlowerThan(first)) return "NO";
+        while (first>last)
+        {
+            first.Move();
+            last.Move();
+        }
+        if (k1 == k2) return "YES";
+        return "NO";
     }
     static void SolveProblem(StreamReader source, StreamWriter destination) {
         string[] tokens_x1 = source.ReadLine().Split(' ');
@@ -28,4 +40,39 @@ public class Solution {
         SolveProblem(stdin, stdout);
         stdout.Flush();
     }
+}
+
+public class Kangaroo 
+{
+    int  _postionIncrement;
+    long _currentPosition;
+    public Kangaroo(int start, int increment)
+    {
+        _currentPosition = start;
+        _postionIncrement = increment;
+    }
+    public long Move() =>
+        _currentPosition += _postionIncrement;
+    public bool SlowerThan(Kangaroo obj) =>
+        _postionIncrement < obj._postionIncrement;
+    public long Position()=>
+        _currentPosition;
+    public int CompareTo(Kangaroo obj) =>
+        _currentPosition.CompareTo(obj.Position());
+    public static bool operator >(Kangaroo obj1, Kangaroo obj2) =>
+        obj1.CompareTo(obj2)>0; 
+    public static bool operator <(Kangaroo obj1, Kangaroo obj2) =>
+        obj1.CompareTo(obj2)<0;
+    public static bool operator !=(Kangaroo obj1, Kangaroo obj2) =>
+        obj1.CompareTo(obj2)!=0;
+    public static bool operator ==(Kangaroo obj1, Kangaroo obj2) =>
+        obj1.CompareTo(obj2)==0;
+    public override bool Equals(Object obj)
+    {
+        if (obj?.GetType() == GetType() )
+            return this == ((Kangaroo)obj);
+        return false;
+    }
+    public override int GetHashCode()=>
+        this._currentPosition.GetHashCode();
 }
